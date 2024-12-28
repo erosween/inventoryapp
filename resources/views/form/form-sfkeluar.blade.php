3b9b1dc0d0cc41d0d6ae0db830e9b858abccf3cb
erosween <robby.syamsuddin@gmail.com>
2024-12-28 09:40:20 +0700

    edit form sf keluar

--- resources/views/form/form-sfkeluar.blade.php
+++ resources/views/form/form-sfkeluar.blade.php
@@ -104,11 +104,10 @@
     </script>
 
 
-    !--Core JS Files-- >
-    <script src="../assets/js/core/jquery.3.2.1.min.js" />
-    < script src="../assets/js/core/popper.min.js">
-
-        <script src="../assets/js/core/bootstrap.min.js"></script>
+    <!--   Core JS Files   -->
+    <script src="../assets/js/core/jquery.3.2.1.min.js"></script>
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
 
     <!-- jQuery UI -->
     <script src="../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
