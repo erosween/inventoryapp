80ab5df91966dfab2a4a2ddcf3a19b430477190c
erosween <robby.syamsuddin@gmail.com>
2025-07-05 09:08:25 +0700

    nambah bulan

--- app/Http/Controllers/HomenocanController.php
+++ app/Http/Controllers/HomenocanController.php
@@ -19,7 +19,7 @@ class HomenocanController extends Controller
 
             // Inisialisasi array bulan dari Juni hingga Desember
             $months = [
-                "Jan" =>1,"Feb" =>2,"Mar" =>3,"Apr" =>4,"May" =>5,"Jun" =>6
+                "Jan" =>1,"Feb" =>2,"Mar" =>3,"Apr" =>4,"May" =>5,"Jun" =>6,"Jul" =>7,"Aug" =>8,"Sept" =>9,"Oct" =>10,"Nov" =>11,"Dec" =>12
 
                 // 'June' => 6, 'July' => 7, 'August' => 8, 
                 // 'September' => 9,'October' => 10, 'November' => 11,'December' => 12
