ad4300b9d8776d545b86f3bb9ee1f722db367c11
erosween <robby.syamsuddin@gmail.com>
2026-08-04 16:35:49 +0700

    Perbaiki sumber data FM-1 Monita Dumai

--- app/Http/Controllers/MonitaDumaiController.php
+++ app/Http/Controllers/MonitaDumaiController.php
@@ -600,8 +600,10 @@ class MonitaDumaiController extends Controller
             ->leftJoin('outlet_performance', 'appsdumais.id_outlet', '=', 'outlet_performance.id_outlet')
             ->select(
                 'appsdumais.*',
+                'outlet_performance.sp_simpati_fm1 as fm1_stsa',
                 'outlet_performance.total_sp_m as m_stsa',
                 'outlet_performance.tgl_update as tgl_sa',
+                'outlet_performance.total_fm1 as fm1_stpv',
                 'outlet_performance.total_m as m_stpv',
                 'outlet_performance.total_m1 as m1_stpv',
                 'outlet_performance.total_mom as mom_stpv',
@@ -650,8 +652,10 @@ class MonitaDumaiController extends Controller
             ->leftJoin('outlet_performance', 'appsdumais.id_outlet', '=', 'outlet_performance.id_outlet')
             ->select(
                 'appsdumais.*',
+                'outlet_performance.sp_simpati_fm1 as fm1_stsa',
                 'outlet_performance.total_sp_m as m_stsa',
                 'outlet_performance.tgl_update as tgl_sa',
+                'outlet_performance.total_fm1 as fm1_stpv',
                 'outlet_performance.total_m as m_stpv',
                 'outlet_performance.total_m1 as m1_stpv',
                 'outlet_performance.total_mom as mom_stpv',
